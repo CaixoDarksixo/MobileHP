f927d3d2406ce89adccb0edb6e14990ca8e29a82
CaixoDarksixo <jpnossol@gmail.com>
2026-06-14 03:24:03 -0300

    fix: rodar o ktlint

--- app/src/main/java/com/example/harrypotterfacts/api/HarryPotterApi.kt
+++ app/src/main/java/com/example/harrypotterfacts/api/HarryPotterApi.kt
@@ -4,15 +4,18 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface HarryPotterApi {
-
     @GET("character/{id}")
-    suspend fun getCharacterById(@Path("id") id: String): List<Character>
+    suspend fun getCharacterById(
+        @Path("id") id: String,
+    ): List<Character>
 
     @GET("characters/staff")
     suspend fun getStaff(): List<Character>
 
     @GET("characters/house/{house}")
-    suspend fun getStudentsByHouse(@Path("house") house: String): List<Character>
+    suspend fun getStudentsByHouse(
+        @Path("house") house: String,
+    ): List<Character>
 
     @GET("spells")
     suspend fun getSpells(): List<Spell>
--- app/src/main/java/com/example/harrypotterfacts/api/Models.kt
+++ app/src/main/java/com/example/harrypotterfacts/api/Models.kt
@@ -10,10 +10,10 @@ data class Character(
     val alternateNames: List<String>,
     val species: String,
     val house: String,
-    val image: String
+    val image: String,
 ) : Serializable
 
 data class Spell(
     val name: String, // [cite: 35]
-    val description: String // [cite: 36]
+    val description: String, // [cite: 36]
 ) : Serializable
